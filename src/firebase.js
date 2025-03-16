b0a4f43cf7efd57d97aac579f1614150246964a1
Don330 <khart1998@outlook.com>
2025-03-16 17:56:23 +1100

    Chaned Firestore details and updated README

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyALS0qLpsEoV6yy6afceIhjxAEnmkBLhP8",
-  authDomain: "disaster-relief-b4a2e.firebaseapp.com",
-  projectId: "disaster-relief-b4a2e",
-  storageBucket: "disaster-relief-b4a2e.firebasestorage.app",
-  messagingSenderId: "593287708958",
-  appId: "1:593287708958:web:90be7d80dab2e380a2090f",
-  measurementId: "G-8KQ17JH8B5"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
